907fc3ac6615b7315df32535db0be603b8410e4d
Javiz7 <31923944+Javiz7@users.noreply.github.com>
2017-09-21 12:17:54 +0200

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,7 +1,7 @@
 public class 1 {
 	public static void main(String[] args) {
-			System.out.println("===================");
-      System.out.println("| This is 1º task |");
-      System.out.println("===================");
+		System.out.println("===================");
+      		System.out.println("| This is 1º task |");
+      		System.out.println("===================");
 	}
 }
